a5cbff470b9f14e5257457c6c005ab2cb02e82c8
jvg87 <jvg87.dev@gmail.com>
2024-02-24 23:03:12 -0300

    chore: add vector icons setup

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,10 +2,13 @@ import React from "react";
 import { Text, View } from "react-native";
 import { styles } from "./styles";
 
+import Feather from "react-native-vector-icons/Feather";
+
 const Home = () => {
 	return (
 		<View style={styles.container}>
 			<Text>Página Home</Text>
+			<Feather name="home" size={25} color="#000000" />
 		</View>
 	);
 };
